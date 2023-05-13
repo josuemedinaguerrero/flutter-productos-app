0167076608d099f9e2f284a9d8ab67756ed213d0
Josué Medina Guerrero <josuemedina.032810@gmail.com>
2023-05-12 21:58:09 -0500

    diseño completado

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,12 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/ui/input_decorations.dart';
+import 'package:productos_app/widgets/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('LoginScreen')),
+    return Scaffold(
+      body: AuthBackground(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 200),
+              CardContainer(
+                child: Column(
+                  children: [
+                    const SizedBox(height: 10),
+                    Text('Login', style: Theme.of(context).textTheme.headlineMedium),
+                    const SizedBox(height: 30),
+                    const _LoginForm(),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 50),
+              const Text(
+                'Crear una nueva cuenta',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 50),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                hintText: 'john.doe@email.com',
+                labelText: 'Correo electrónico',
+                prefixIcon: Icons.alternate_email,
+              ),
+            ),
+            const SizedBox(height: 30),
+            TextFormField(
+              autocorrect: false,
+              obscureText: true,
+              decoration: InputDecorations.authInputDecoration(
+                hintText: '*********',
+                labelText: 'Contraseña',
+                prefixIcon: Icons.lock_outline,
+              ),
+            ),
+            const SizedBox(height: 30),
+            MaterialButton(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              disabledColor: Colors.grey,
+              elevation: 0,
+              color: Colors.deepPurple,
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                child: const Text('Ingresar', style: TextStyle(color: Colors.white)),
+              ),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
